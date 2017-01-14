9446b1b7b6edb379f0ac696055b6b4f2531d9565
Michael Scherbaum <mscherbaum@salesforce.com>
2017-01-14 10:29:37 +0000

    Added Logger

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,3 +3,6 @@ require 'rspotify/oauth'
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :spotify, ENV['SPOTIFY_CLIENTID'],ENV['SPOTIFY_SECRET'], scope: 'user-read-email playlist-modify-public user-library-read user-library-modify'
 end
+
+OmniAuth.config.logger = Logger.new(STDOUT)
+OmniAuth.logger.progname = "omniauth"
