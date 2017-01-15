9bc09f25c11a4c69513afcf0261d8fbbf08e9724
Michael Scherbaum <mscherbaum@salesforce.com>
2017-01-15 07:15:55 +0000

    Setting up logging

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,9 @@
 class WelcomeController < ApplicationController
+
+def index
+	logger.debug "This is a Debug message"
+	logger.info "This is an Info message"
+	logger.warn "This is an Warn message"
+end
+
 end
--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module RubySpotifyTransfer
     RSpotify.authenticate(ENV['SPOTIFY_CLIENTID'],ENV['SPOTIFY_SECRET'])    
     OmniAuth.config.logger = Rails.logger
     config.logger = Logger.new(STDOUT)
-    config.log_level = :debug
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
